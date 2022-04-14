abfcc2a65733ec1d60036185307edc558fd88ca4
mdsohag65 <sohag92ahmed@gmail.com>
2022-04-14 11:48:26 +0600

    require auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CheckOut from './images/CheckOut/CheckOut/CheckOut';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
+import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -21,6 +23,11 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/checkout' element={
+          <RequireAuth>
+            <CheckOut></CheckOut>
+          </RequireAuth>
+        }></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
